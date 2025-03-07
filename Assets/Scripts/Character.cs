d9a28870ed565eb9991427f3a84784fbd94e0b1a
Mcread1019 <143539294+Mcread1019@users.noreply.github.com>
2025-03-06 19:37:23 -0500

    needs camera fixing

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Charcter 
+{
+
+    [SerializeField] enum Characterclass;
+    
+    [SerializeField] enum charcterType;
+
+
+    [SerializeField] enum AbilityNameScore;
+
+    struct AbilityScore
+    {
+        int Strength;
+        int Dexterity;
+        int Constitution;
+        int Intelligence;
+        int Wisdom;
+        int Charisma;
+    }
+
+    
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enum.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enum : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    enum Charcterclass {Archer, Warrior, Barbarian, Bard };
+    enum CharcterType {Human, Elf, Orc, Dwarf };
+
+    enum AbilityScoreName {Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
